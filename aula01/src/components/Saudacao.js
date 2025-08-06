c9c8ef118879a0032724ad5a43ed811ccbead72b
DANIELI DE CAMPOS <danieli.de04@cs.up.edu.br>
2025-08-05 21:02:01 -0300

    add(aula)

---
+++ aula01/src/components/Saudacao.js
@@ -0,0 +1,14 @@
+import { Text, StyleSheet } from "react-native";
+
+export default function(){
+    return (
+        <Text>Olá Alex seja bem vindo!</Text>
+    );
+}
+
+const Estilo = StyleSheet.create({
+    textoSaudacao: {
+        fontSize: 20,
+        color: '#90b' 
+    }
+})
